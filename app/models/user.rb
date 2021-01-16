175fd539003f6fd67b0b41f2706f9dd3675b1124
Trynet123 <su508102@yahoo.co.jp>
2021-01-16 15:36:53 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birth_day
+    validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  end
 end
