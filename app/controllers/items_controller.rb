00045da517e18565e13f49bed6388054398535dd
Trynet123 <su508102@yahoo.co.jp>
2021-01-18 09:24:06 +0900

    商品情報DBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
   end
 
@@ -7,8 +9,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    @item = Item.create(item_params)
+    if @item.valid?
       redirect_to root_path
     else
       render new_item_path
@@ -17,7 +19,9 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :text, :category_id, :condition_id, :shipping_charge_id, :presence, :days_to_ship_id, :price, :image)
+    params.require(:item).permit(
+      :name, :text, :prefecture_id, :category_id, :condition_id, :shipping_charge_id, :presence, :days_to_ship_id, :price, :image
+    ).merge(user_id: current_user.id)
   end
 
 end
