738a68287d5d9799393934842883c1f97b62285c
Trynet123 <su508102@yahoo.co.jp>
2021-01-18 09:50:38 +0900

    itemモデル単体テスト雛形

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe "商品出品機能" do
+    it "あaa" do
+      
+    end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+    # it '' do
+      
+    # end
+  end
 end
