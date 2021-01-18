53d069c911e2b17afe0f47c5aee5640503094c76
Trynet123 <su508102@yahoo.co.jp>
2021-01-18 14:01:06 +0900

    Itemモデル単体テストの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,21 +9,24 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :prefecture_id, numericality: { other_than: 1}
-  with_options presence: true do
-    validates :name
-    validates :text
+  with_options numericality: { other_than: 1} do
     validates :category_id 
     validates :condition_id
     validates :shipping_charge_id
     validates :days_to_ship_id
+    validates :prefecture_id
+  end
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
     validates :price, 
       numericality: {
         greater_than_or_equal_to: 300, 
         less_than_or_equal_to: 9999999, 
         only_integer: true, 
         message: "Out of setting range"
-      },
-      format: { with: /\A[0-9]+\z/, message: "Half-width number"}
+      }
   end
 end
