eaa053a921965ca477a18bdd68596fbb9e8f012c
Trynet123 <su508102@yahoo.co.jp>
2021-01-21 11:45:23 +0900

    Formオブジェクトorderモデル作成、item_transactionsテーブルとaddressesテーブルの作成

---
+++ app/models/order.rb
@@ -0,0 +1,17 @@
+class Order
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :phone_number, :building_name
+
+  # Addresモデルのバリデーション
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: ""}
+    validates :house_number
+    validates :phone_number, format: { with: /\A[0-9]+\z/, message: ""}
+  end
+
+  def save
+    item_transaction = ItemTransaction.create(item_id: item.id, user_id: user.id)
+  end
+end
