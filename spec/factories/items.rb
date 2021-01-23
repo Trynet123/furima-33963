bb85d6f9e43f793b98de4f4d115a6528e55696d1
Trynet123 <su508102@yahoo.co.jp>
2021-01-23 14:55:55 +0900

    テストコード修正imageについて
    テストコード読み込み時mysqlエラーが発生したのでsleepメソッドにて、１秒待機させることで解消。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,5 +10,9 @@ FactoryBot.define do
     price { '300' }
     user_id { 1 }
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Order, type: :model do
       @user = FactoryBot.create(:user)
       @item = FactoryBot.create(:item)
       @order = FactoryBot.build(:order, user_id: @user.id, item_id: @item.id)
+      sleep(1)
     end
 
     it '必須項目を正しく入力したら登録できること' do
