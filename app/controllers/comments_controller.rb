46b2388b0d13d60b54b45ab4460b82fd7363f2a2
Trynet123 <su508102@yahoo.co.jp>
2021-02-08 17:50:19 +0900

    新規投稿画面表示、チャンネル作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
   def new
+    # @comments = Comment.include(:user)
+    @comments = Comment.all
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(text: params[:comment][:text])
   end
 end
