0af3a487f3ee8fbcec6697061b177708f55e1380
Trynet123 <su508102@yahoo.co.jp>
2021-02-08 20:52:38 +0900

    ルーティング変更、アソシエーション設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
     resources :orders, only: [:index, :create]
+    resources :comments, only: :create
   end
-  resources :comments, only: [:new, :create]
 end
